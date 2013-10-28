952656e9829e4bad3af362050ca75ca8c95a7842
skc4192 <skc4192@yahoo.com>
2013-10-28 08:57:02 -0600

    Update appsetup.rb
    another test

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -5,6 +5,8 @@ node[:deploy].each do |app_name, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar install
     chown -R apache:apache *
     echo "shawn" >> shawn.txt
     EOH
