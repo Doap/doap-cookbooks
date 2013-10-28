756fa615d61ea69e125c43fc67b4e78d1b49eddf
skc4192 <skc4192@yahoo.com>
2013-10-28 08:40:11 -0600

    Update appsetup.rb
    wtf, trying chown instead of default script..

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -5,29 +5,9 @@ node[:deploy].each do |app_name, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar install
+    chown -R apache:apache *
     EOH
   end
-
-  template "#{deploy[:deploy_to]}/current/db-connect.php" do
-    source "db-connect.php.erb"
-    mode 0660
-    group deploy[:group]
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")   
-      owner "apache"
-    end
-
-    variables(
-      :host =>     (deploy[:database][:host] rescue nil),
-      :user =>     (deploy[:database][:username] rescue nil),
-      :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil),
-      :table =>    (node[:phpapp][:dbtable] rescue nil)
-    )
 
    only_if do
      File.directory?("#{deploy[:deploy_to]}/current")
